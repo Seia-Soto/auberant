c65eb0cf164cdce32c37dfee211d1f253df8690a
Equalise <30369714+artwork-if@users.noreply.github.com>
2018-02-06 05:40:54 +0900

    From Push!

--- event/ready.js
+++ event/ready.js
@@ -2,6 +2,6 @@ const int = require('../int.json')
 module.exports = client => {
   console.log(`Signed as ${client.user.tag}`)
 
-  client.user.setActivity(`${int.activity}, { url: 'https://auberant.tk', type: 'STREAMING' }`)
+  client.user.setActivity(`${int.activity}`)
   console.log(`Client user activity setted as ${int.activity}`)
 }
