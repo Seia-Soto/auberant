20c70445569da0e98d73afae3d6f29dc82d294a7
Equalise <30369714+artwork-if@users.noreply.github.com>
2018-02-08 00:40:13 +0900

    Update load.js

--- react/load.js
+++ react/load.js
@@ -11,141 +11,120 @@ exports.run = (client, message, args) => {
       description: `Loading.\n**5%** [#....................]`
     }
     })
-  }).then((load) => {
     load.edit({embed: {
       color: 3447003,
       title: 'load',
       description: `Loading..\n**10%** [##...................]`
     }
     })
-  }).then((load) => {
     load.edit({embed: {
       color: 3447003,
       title: 'load',
       description: `Loading...\n**15%** [###..................]`
     }
     })
-  }).then((load) => {
     load.edit({embed: {
       color: 3447003,
       title: 'load',
       description: `Loading.\n**20%** [####.................]`
     }
     })
-  }).then((load) => {
     load.edit({embed: {
       color: 3447003,
       title: 'load',
       description: `Loading..\n**25%** [#####................]`
     }
     })
-  }).then((load) => {
     load.edit({embed: {
       color: 3447003,
       title: 'load',
       description: `Loading...\n**30%** [######...............]`
     }
     })
-  }).then((load) => {
     load.edit({embed: {
       color: 3447003,
       title: 'load',
       description: `Loading.\n**35%** [#######..............]`
     }
     })
-  }).then((load) => {
     load.edit({embed: {
       color: 3447003,
       title: 'load',
       description: `Loading..\n**40%** [########.............]`
     }
     })
-  }).then((load) => {
     load.edit({embed: {
       color: 3447003,
       title: 'load',
       description: `Loading...\n**45%** [#########............]`
     }
     })
-  }).then((load) => {
     load.edit({embed: {
       color: 3447003,
       title: 'load',
       description: `Loading.\n**50%** [##########...........]`
     }
     })
-  }).then((load) => {
     load.edit({embed: {
       color: 3447003,
       title: 'load',
       description: `Loading..\n**55%** [###########..........]`
     }
     })
-  }).then((load) => {
     load.edit({embed: {
       color: 3447003,
       title: 'load',
       description: `Loading...\n**60%** [############.........]`
     }
     })
-  })
-  .then((load) => {
     load.edit({embed: {
       color: 3447003,
       title: 'load',
       description: `Loading.\n**65%** [#############........]`
     }
     })
-  }).then((load) => {
     load.edit({embed: {
       color: 3447003,
       title: 'load',
       description: `Loading..\n**70%** [###############......]`
     }
     })
-  }).then((load) => {
     load.edit({embed: {
       color: 3447003,
       title: 'load',
       description: `Loading...\n**75%** [################.....]`
     }
     })
-  }).then((load) => {
     load.edit({embed: {
       color: 3447003,
       title: 'load',
       description: `Loading.\n**80%** [#################....]`
     }
     })
-  }).then((load) => {
     load.edit({embed: {
       color: 3447003,
       title: 'load',
       description: `Loading..\n**85%** [##################...]`
     }
     })
-  }).then((load) => {
     load.edit({embed: {
       color: 3447003,
       title: 'load',
       description: `Loading...\n**90%** [###################..]`
     }
     })
-  }).then((load) => {
     load.edit({embed: {
       color: 3447003,
       title: 'load',
       description: `Loading.\n**95%** [####################.]`
     }
     })
-  }).then((load) => {
     load.edit({embed: {
       color: 3447003,
       title: 'load',
       description: `Loading..\n**100%** [#####################]`
     }
     })
-  }).then((load) => {
     load.edit({embed: {
       color: 3447003,
       title: 'load',
