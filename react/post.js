a1719ca81df34a2be82a25805bd55614d81210d7
Equalise <30369714+artwork-if@users.noreply.github.com>
2018-02-08 01:13:51 +0900

    Update post.js

--- react/post.js
+++ react/post.js
@@ -1,6 +1,6 @@
 exports.run = (client, message, args) => {
   var messageToNotice = args.slice(0).join(' ')
-  client.guilds.forEach( guild => guild.channels.find(c=> c.permissionsFor(guild.me).has("SEND_MESSAGES").send({embed: {
+  client.guilds.forEach( guild => guild.channels.send({embed: {
     color: 3447003,
     title: 'post',
     description: `${messageToNotice}`
