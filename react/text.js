0b3d533c9c3b9a23b5767acf9252705c8eef0118
Equalise <30369714+artwork-if@users.noreply.github.com>
2018-02-07 18:32:05 +0900

    Add files via upload

---
+++ react/text.js
@@ -0,0 +1,51 @@
+exports.run = (client, message, args) => {
+  if (!args[0] || !args[1]) {
+    message.channel.send({embed: {
+      color: 3447003,
+      title: 'Text',
+      description: `Send ASCII text (font: ANSI Shadow). Argument is string.`
+    }
+    })
+    return
+  } else if (args[0] == 'font-list') {
+    message.channel.send({embed: {
+      color: 3447003,
+      title: 'Text',
+      description: `These are list of fonts!\n\n`
+    }
+    })
+    return
+  }
+  const figlet = require('figlet')
+  figlet.text(`${args[1]}`, {
+    font: 'ANSI Shadow',
+    horizontalLayout: 'default',
+    verticalLayout: 'default'
+  } (error, data) => {
+    if (error) {
+      console.error(error)
+      message.channel.send({embed: {
+        color: 3447003,
+        title: 'Error',
+        description: `${error}`
+      }
+      })
+      return
+    }
+    message.channel.send('```' + data + '```')
+  })
+
+}
+
+exports.conf = {
+  enabled: true,
+  guildOnly: true,
+  aliases: ['word'],
+  permLevel: 0
+}
+
+exports.help = {
+  name: 'text',
+  description: 'Print text to ascii artwork',
+  usage: 'text <word>'
+}
