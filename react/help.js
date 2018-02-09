01220d4fb0d903ac35402aa74f1289639a1bd7ba
Equalise <30369714+artwork-if@users.noreply.github.com>
2018-02-09 20:03:33 +0900

    Update help.js

--- react/help.js
+++ react/help.js
@@ -2,14 +2,14 @@ exports.run = (client, message, args) => {
   message.channel.send({embed: {
     color: 3447003,
     title: 'help',
-    description: `With ${client.users.size} people in ${client.guilds.size} servers!`
+    description: `**With ${client.users.size} people in ${client.guilds.size} servers!\n[Documentation (https://auberant.tk)](https://auberant.tk/products/bot)`
   }})
 }
 
 exports.conf = {
   enabled: true,
   guildOnly: true,
-  aliases: ['docs'],
+  aliases: ['docs', '도움'],
   permLevel: 0
 }
 
