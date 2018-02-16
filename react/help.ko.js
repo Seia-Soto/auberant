b4d4180679823cb00498ee27f16c86310c91bdad
Equalise <30369714+artwork-if@users.noreply.github.com>
2018-02-16 20:14:55 +0900

    Add files via upload

--- react/help.ko.js
+++ react/help.ko.js
@@ -2,7 +2,7 @@ exports.run = (client, message, args) => {
   message.channel.send({embed: {
     color: 3447003,
     title: 'help',
-    description: `Auberant 1.2.1, ${client.guild.size}개의 서버들과 함께 있습니다!\n[웹사이트나 문서](https://auberant.tk)를 찾고 계신가요?\n\n
+    description: `Auberant 1.2.1, ${client.guilds.size}개의 서버들과 함께 있습니다!\n[웹사이트나 문서](https://auberant.tk)를 찾고 계신가요?\n\n
                   ;도움말 *Everyone*\n
                   ;노트 *Everyone*\n
                   ;질의 *Everyone*\n
