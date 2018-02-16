62b1d6f8a2e8db7f90f6c844f3d0e411883b6ddb
Equalise <30369714+artwork-if@users.noreply.github.com>
2018-02-16 17:12:14 +0900

    Add files via upload

---
+++ react/ping.ko.js
@@ -0,0 +1,27 @@
+exports.run = (client, message, args) => {
+  message.channel.send({embed: {
+    color: 3447003,
+    title: '질의',
+    description: '요청 중..'
+  }}).then((ping) => {
+    ping.edit({embed: {
+      color: 3447003,
+      title: '질의',
+      description: `요청이 반환되었습니다! ${ping.createdTimestamp - message.createdTimestamp}ms가 소요되었습니다.`
+    }})
+  })
+  .catch(console.error)
+}
+
+exports.conf = {
+  enabled: true,
+  guildOnly: true,
+  aliases: ['질의'],
+  permLevel: 0
+}
+
+exports.help = {
+  name: 'ping-ko',
+  description: 'Load Auberant API respones time.',
+  usage: 'ping-ko'
+}
