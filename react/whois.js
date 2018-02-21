891a77975062d8c98cdf26f7a031a07b746edf3f
Equalise <30369714+artwork-if@users.noreply.github.com>
2018-02-21 17:40:21 +0900

    Add files via upload

--- react/whois.js
+++ react/whois.js
@@ -1,4 +1,4 @@
-const whois = require('whois')
+const whois = require('whois-api')
 exports.run = (client, message, args) => {
   if (!args[0]) {
     message.channel.send({embed: {
@@ -7,8 +7,8 @@ exports.run = (client, message, args) => {
       description: `Query domain registeration information from ICANN WHOIS. Argument is domain name.`
     }})
   } else {
-    whois.lookup(args[0], (error, data) => {
-      console.log(data)
+    whois.rawLookup(args[0], (error, result) => {
+      console.log(result)
     })
   }
 }
