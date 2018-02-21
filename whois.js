b3a959db7cfaf7d591fb12b6f9eb403a09a3cf7f
Equalise <30369714+artwork-if@users.noreply.github.com>
2018-02-21 19:57:50 +0900

    Add files via upload

---
+++ whois.js
@@ -0,0 +1,61 @@
+const translate = require('google-translate-api')
+const whois = require('whois')
+const int = require('../int.json')
+exports.run = (client, message, args) => {
+  const react = message.content.split(' ')[0].slice(int.prefix.length)
+  translate(react, {to: 'en'}).then(res => {
+    switch (res.from.language.iso) {
+      case 'en':
+        if (!args[0]) {
+          message.channel.send({embed: {
+            color: 3447003,
+            title: 'whois',
+            description: `Query domain registeration information from ICANN WHOIS. Argument is domain name.`
+          }})
+        } else {
+          whois.lookup(args[0], (error, result) => {
+            console.log(result)
+            var result = substring(0, 2047)
+            message.channel.send({embed: {
+              color: 3447003,
+              title: 'whois',
+              description: `${result}`
+            }})
+          })
+        }
+        break;
+      case 'ko':
+        if (!args[0]) {
+          message.channel.send({embed: {
+            color: 3447003,
+            title: '후이즈',
+            description: `등록된 도메인 정보를 ICANN WHOIS에서 가져옵니다. 인수는 도메인 이름입니다.`
+          }})
+        } else {
+          whois.lookup(args[0], (error, result) => {
+            console.log(result)
+            var result = substring(0, 2047)
+            message.channel.send({embed: {
+              color: 3447003,
+              title: '후이즈',
+              description: `${result}`
+            }})
+          })
+        }
+        break;
+    }
+  })
+}
+
+exports.conf = {
+  enabled: true,
+  guildOnly: true,
+  aliases: ['후이즈'],
+  permLevel: 0
+}
+
+exports.help = {
+  name: 'whois',
+  description: 'Query domain registeration information from ICANN WHOIS.',
+  usage: 'whois'
+}
