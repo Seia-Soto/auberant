05c2d3648b5c61a000445fba42e72a910c42df52
Equalise <30369714+artwork-if@users.noreply.github.com>
2018-02-26 06:11:21 +0900

    Add files via upload

--- react/push.js
+++ react/push.js
@@ -1,9 +1,31 @@
 exports.run = (client, message, args) => {
-  message.channels.find('name', 'push').send({embed: {
+  if (args[0] === 'to:here') {
+    message.guild.channels.find('name', 'push').send({embed: {
+      color: 4620980,
+      title: args.slice(1).join(' ').split(' content:')[0],
+      description: args.slice(1).join(' ').split(' content:')[1]
+    }})
+    return
+  }
+  var guildList = client.guilds.array()
+  try {
+    guildList.forEach(guild => message.guild.channels.find('name', 'push').send({embed: {
+      color: 4620980,
+      title: args.slice(1).join(' ').split(' content:')[0],
+      description: args.slice(1).join(' ').split(' content:')[1]
+    }}))
+  } catch (error) {
+    message.channel.send({
+      color: 4620980,
+      title: 'push',
+      description: `Could not send push notifications to __${guild.name}__!`
+    })
+  }
+  message.channel.send({
     color: 4620980,
-    title: `${args.slice(0).join(' ').split(' content:')[0]}`,
-    description: `${args.slice(0).join(' ').split(' content:')[1]}`
-  }})
+    title: 'push',
+    description: 'Notifications are pushed to servers!'
+  })
 }
 
 exports.conf = {
