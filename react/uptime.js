21b81e7aa2e66cbffef333a24ea6c155b632e30c
Let <30369714+artwork-if@users.noreply.github.com>
2018-03-09 18:02:50 +0900

    Update uptime.js

--- react/uptime.js
+++ react/uptime.js
@@ -18,14 +18,14 @@ exports.run = (client, message, args) => {
         message.channel.send({embed: {
               color: 4620980,
               title: '업타임',
-              description: `${process.env}`
+              description: `${process.uptime()}`
             }})
         break;
       default:
         message.channel.send({embed: {
               color: 4620980,
               title: 'uptime',
-              description: `${process.uptime}`
+              description: `${process.uptime()}`
             }})
         break;
     }
