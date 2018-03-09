52934a85ba16b2c7ff91eba9d3863308b65e4add
Let <30369714+artwork-if@users.noreply.github.com>
2018-03-09 17:50:15 +0900

    Update uptime.js

--- react/uptime.js
+++ react/uptime.js
@@ -1,1 +1,36 @@
-//
+const translate = require('google-translate-api')
+const int = require('../int.json')
+exports.run = (client, message, args) => {
+  const react = message.content.split(' ')[0].slice(int.prefix.length)
+  translate(react, {to: 'en'}).then(res => {
+    switch (res.from.language.iso) {
+      case 'ko':
+        message.channel.send({embed: {
+              color: 4620980,
+              title: '업타임',
+              description: `${require('os').uptime()}`
+            }})
+        break;
+      default:
+        message.channel.send({embed: {
+              color: 4620980,
+              title: 'uptime',
+              description: `${require('os').uptime()}`
+            }})
+        break;
+    }
+  })
+}
+
+exports.conf = {
+  enabled: true,
+  guildOnly: true,
+  aliases: ['업타임'],
+  permLevel: 0
+}
+
+exports.help = {
+  name: 'uptime',
+  description: 'Get uptime of Auberant.',
+  usage: 'uptime'
+}
