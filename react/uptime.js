839f9292cfc78a1e62b4fd2f8da40f37127eeb54
Let <30369714+artwork-if@users.noreply.github.com>
2018-03-09 18:21:07 +0900

    Update uptime.js

--- react/uptime.js
+++ react/uptime.js
@@ -1,19 +1,5 @@
 const translate = require('google-translate-api')
 const int = require('../int.json')
-String.prototype.toHHMMSS = () => {
-    var sec_num = parseInt(this, 10); // don't forget the second param
-    var hours = Math.floor( (seconds - (days*86000)) / 3600 )
-    var minutes = Math.floor((sec_num - (hours * 3600)) / 60);
-    var seconds = sec_num - (hours * 3600) - (minutes * 60);
-
-    if (hours   < 10) {hours   = "0"+hours;}
-    if (minutes < 10) {minutes = "0"+minutes;}
-    if (seconds < 10) {seconds = "0"+seconds;}
-    var time    = hours+':'+minutes+':'+seconds;
-    return time;
-}
-var time = process.uptime()
-var uptime = (time + "").toHHMMSS()
 exports.run = (client, message, args) => {
   const react = message.content.split(' ')[0].slice(int.prefix.length)
   translate(react, {to: 'en'}).then(res => {
@@ -22,14 +8,14 @@ exports.run = (client, message, args) => {
         message.channel.send({embed: {
               color: 4620980,
               title: '업타임',
-              description: `${uptime}`
+              description: `${uptime} 초`
             }})
         break;
       default:
         message.channel.send({embed: {
               color: 4620980,
               title: 'uptime',
-              description: `${uptime}`
+              description: `${uptime} second`
             }})
         break;
     }
