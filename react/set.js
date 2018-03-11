60cc45a2f1bc7feb317d2c89534e7d241c62bc74
Let <30369714+artwork-if@users.noreply.github.com>
2018-03-11 19:55:43 +0900

    Update set.js

--- react/set.js
+++ react/set.js
@@ -1,1 +1,30 @@
+const translate = require('google-translate-api')
+const braille = require('braille')
+const int = require('../int.json')
+exports.run = (client, message, args) => {
+  const react = message.content.split(' ')[0].slice(int.prefix.length)
+  var setValueTo = args.slice(1).join(" ")
+  switch(args[0]) {
+    case 'activity':
+      client.user.setActivity(`${setValueTo}`)
+      console.log(`Client user's activity setted to **${setValueTo}**.`)
+      message.reply(`Client user's activity setted to **${setValueTo}**.`)
+      break;
+    default:
+      message.reply('No entries')
+      break;
+  }
+}
 
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: ['settings'],
+  permLevel: 4
+}
+
+exports.help = {
+  name: 'set',
+  description: 'Configure bot settings',
+  usage: 'set <topic> <value>'
+}
