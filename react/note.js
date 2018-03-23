95e1ef0bf9239027a3a8a64db929eb1438d02820
Equalise <30369714+artwork-if@users.noreply.github.com>
2018-03-23 16:36:53 +0900

    Update note
    이제 노트 명령어를 다시 사용하실 수 있습니다!

--- react/note.js
+++ react/note.js
@@ -1,97 +1,71 @@
 const Enmap = require('enmap')
 const EnmapLevel = require('enmap-level')
 const Notes = new Enmap({ provider: new EnmapLevel({ name: 'note' }) })
-const Discord = require('discord.js')
-const translate = require('google-translate-api')
-const int = require('../int.json')
-exports.run = (client, message, args) => {
-  const react = message.content.split(' ')[0].slice(int.prefix.length)
-  translate(react, {to: 'en'}).then(res => {
-    switch (res.from.language.iso) {
-      case 'ko':
-        if (!args[0]) {
-          message.channel.send({embed: {
-            color: 4620980,
-            title: '노트',
-            description: 'Auberant에서 노트를 써보세요. 먼저 노트를 만들어보세요: ``;노트 새로만들기 <내용>``. 그리고 ``;노트 열기 <ID>``로 열어보세요.\n\n노트는 Auberant가 업데이트할 때 언제든지 사라질 수 있습니다. 비밀번호 또는 개인정보를 Auberant에 저장하지 마세요. 저희는 유실된 노트 또는 개인정보에 대해 책임지지 않습니다. :P'
-          }})
-          return
+exports.run = (client, message, int, args, arg, perms, requestLanguage) => {
+  let messageTitle = `Note`
+  let messageDescription = `Take notes on Auberant. Just try: ``;note new <content>``. And open it with ``;note open <ID>``.\n\nA note can disappear when Auberant get new update. Do not keep your password or private thing on Auberant. We are not responsible for any leaked personal information or deleted notes. :P`
+  let messageDescription_nocontent = `Can't find any content to save.`
+  let messageDescription_saved = `Now you can find your note by using **;note open ${_id}**.`
+  let messageDescription_noid = `Can't interpret id to open.`
+  let errorTitle = `Error`
+  let _new = `new`
+  let _open = `open`
+  // Various
+  let _id = Math.floor(Math.random() * 9999) + 1
+  switch (requestLanguage) {
+    case `ko`:
+      messageTitle = `노트`
+      messageDescription = `Auberant에서 노트를 써보세요. 먼저 노트를 만들어보세요: ``;노트 새로만들기 <내용>``. 그리고 ``;노트 열기 <ID>``로 열어보세요.\n\n노트는 Auberant가 업데이트할 때 언제든지 사라질 수 있습니다. 비밀번호 또는 개인정보를 Auberant에 저장하지 마세요. 저희는 유실된 노트 또는 개인정보에 대해 책임지지 않습니다. :P`
+      messageDescription_nocontent = `저장할 내용을 찾을 수 없습니다.`
+      messageDescription_saved = `이제 저장된 노트를 **;노트 열기 ${_id}**로 찾을 수 있습니다.`
+      messageDescription_noid = ``
+      errorTitle = `오류`
+      _new = `새로만들기`
+      _open = `열기`
+      break;
+  }
+  switch (args[0]) {
+    case `${_new}`:
+      if (!args.slice(1).join(' ')) {
+        message.channel.send({embed: {
+          color: 4620980,
+          title: `${messageTitle}`,
+          description: `${messageDescription_nocontent}`
+        }})
+        return
+      }
+      Notes.set(`${_id}`, `${args.slice(1).join(' ')}`)
+      message.channel.send({embed: {
+        color: 4620980,
+        title: `${messageTitle}`,
+        description: `${messageDescription_saved}`
+      }})
+      break;
+    case `${_open}`:
+      if (!args[1]) {
+        message.channel.send({embed: {
+          color: 4620980,
+          title: `${messageTitle}`,
+          description: `${messageDescription_noid}`
+        }})
+        return
+      }
+      message.channel.send({embed: {
+        color: 4620980,
+        title: `${messageTitle}`,
+        description: `${Notes.open(`${args[1]}`)}`
+      }})
+      break;
+    default:
+      message.channel.send({embed: {
+        color: 4620980,
+        title: `${messageTitle}`,
+        description: `${messageDescription}`,
+        footer: {
+          text: `#${args[1]}`
         }
-        switch (args[0]) {
-          case '새로만들기':
-            var contentToNote = args.slice(1).join(' ')
-            var contentHeaderID = Math.floor(Math.random() * 9999) + 1
-            Notes.set(`${contentHeaderID}`, `${contentToNote}`)
-
-            message.channel.send({embed: {
-              color: 4620980,
-              title: '노트',
-              description: `이제 저장된 노트를 Auberant가 있는 어디에서나 열 수 있습니다! 노트의 ID는 **${contentHeaderID}**입니다. 다른 누군가가 이 ID를 사용하지 않기를 빌죠...`
-            }})
-            break;
-          case '열기':
-            if (!args[1]) {
-              message.channel.send({embed: {
-                color: 4620980,
-                title: '노트',
-                description: `예상치 않은 문제가 생겼어요! 노트의 ID가 주어지지 않았잖아요. :P`
-              }})
-            } else {
-              message.channel.send({embed: {
-                color: 4620980,
-                title: '노트',
-                description: `${Notes.get(args[1])}`,
-                footer: {
-                  text: `#${args[1]}`
-                }
-              }})
-            }
-            break;
-        }
-        break;
-      default:
-        if (!args[0]) {
-          message.channel.send({embed: {
-            color: 4620980,
-            title: 'note',
-            description: 'Take notes on Auberant. Just try: ``;note new <content>``. And open it with ``;note open <ID>``.\n\nA note can disappear when Auberant get new update. Do not keep your password or private thing on Auberant. We are not responsible for any leaked personal information or deleted notes. :P'
-          }})
-          return
-        }
-        switch (args[0]) {
-          case 'new':
-            var contentToNote = args.slice(1).join(' ')
-            var contentHeaderID = Math.floor(Math.random() * 9999) + 1
-            Notes.set(`${contentHeaderID}`, `${contentToNote}`)
-
-            message.channel.send({embed: {
-              color: 4620980,
-              title: 'note',
-              description: `Now you can open your saved notes anywhere you have Auberant! Note ID is **${contentHeaderID}**. I do not want anyone else to use that ID ...`
-            }})
-            break;
-          case 'open':
-            if (!args[1]) {
-              message.channel.send({embed: {
-                color: 4620980,
-                title: 'note',
-                description: `There was a problem fetching your notes. The name of the note to import is missing! :P`
-              }})
-            } else {
-              message.channel.send({embed: {
-                color: 4620980,
-                title: 'note',
-                description: `${Notes.get(args[1])}`,
-                footer: {
-                  text: `#${args[1]}`
-                }
-              }})
-            }
-            break;
-        }
-        break;
-    }
-  })
+      }})
+  }
 }
 
 exports.conf = {
