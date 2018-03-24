3382fb691a22d9540d99d106bebfa58f247e3cee
Equalise <30369714+artwork-if@users.noreply.github.com>
2018-03-24 23:32:59 +0900

    whois updated before update

---
+++ react/whois.js
@@ -0,0 +1,26 @@
+exports.run = (client, message, int, args, arg, perms, requestLanguage) => {
+  // messageDescriptions
+  let messageTitle = `Whois`
+  let messageDescription = `Query WHOIS databases. Argument is string.`
+  let errorTitle = `Error`
+  switch (requestLanguage) {
+    case `ko`:
+      messageTitle = `후이즈`
+      messageDescription = `후이즈 데이터베이스를 쿼리합니다. 안수는 문자열입니다.`
+      errorTitle = `오류`
+      break;
+  }
+  message.channel.send({embed: {
+    color: 4620980,
+    title: `${messageTitle}`,
+    description: `${messageDescription}`
+  }})
+}
+
+exports.conf = {
+  enabled: true,
+  guildOnly: true,
+  aliases: [`후이즈`],
+  permLevel: 0,
+  name: `whois`
+}
