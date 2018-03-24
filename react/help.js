a2c9d8261b4bc86b09439d46088eb9a0caf8fe6a
Equalise <30369714+artwork-if@users.noreply.github.com>
2018-03-24 14:46:49 +0900

    editted

--- react/help.js
+++ react/help.js
@@ -7,8 +7,8 @@ Let made Auberant with LuminescentLight and there are 2 contributors.\n
 Open-distribution: ${int.version}-${int.build} build (${int.date})
 Here you are! If you need [Auberant for your server](https://discordapp.com/api/oauth2/authorize?client_id=410170772599078913&permissions=8&scope=bot) or [link of support server](https://discord.gg/YzBZNQq)!\n
 **1. Ascii** - send ascii artwork
-**2. Avatar** - send link of your avatar
-**3. help** - send Auberant's help documentation
+**2. Help** - send Auberant's help documentation
+**3. Note** - write and save it to Auberant
 **Other commands are on maintences! Wait for moments. You can execute each commands to get more informations.**
 `
   let errorTitle = `Error`
@@ -21,8 +21,8 @@ Let은 Auberant를 LuminescentLight 외 2명의 기여자와 함께 만들었습
 커뮤니티 배포버전: ${int.version}-${int.build} 빌드 (${int.date})
 만약 [당신의 서버를 위한 Auberant](https://discordapp.com/api/oauth2/authorize?client_id=410170772599078913&permissions=8&scope=bot)가 필요하시거나 [지원 서버의 주소](https://discord.gg/YzBZNQq)가 필요하신가요?\n
 **1. 아스키** - 아스키 삽화를 출력합니다
-**2. 프로필사진** - 프로필사진의 주소를 출력합니다
-**3. 도움말** - Auberant의 도움말을 출력합니다
+**2. 도움말** - Auberant의 도움말을 출력합니다
+**3. 노트** - 작성 후 Auberant에 저장합니다
 **다른 명령어는 현재 수리 중에 있습니다. 각 명령어를 실행함으로서 더욱 자세한 정보를 얻을 수 있습니다.**
 `
       errorTitle = `오류`
