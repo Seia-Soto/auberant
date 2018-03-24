f7deecfd834224e4f6c682d0be145393ef657f6e
Equalise <30369714+artwork-if@users.noreply.github.com>
2018-03-24 15:03:44 +0900

    Just for next version!
    1.2.2 is comming

--- react/note.js
+++ react/note.js
@@ -9,7 +9,7 @@ exports.run = (client, message, int, args, arg, perms, requestLanguage) => {
   let messageTitle = `Note`
   let messageDescription = `Take notes on Auberant. Just try: \`\`;note new <content>\`\`. And open it with \`\`;note open <ID>\`\`.\n\nA note can disappear when Auberant get new update. Do not keep your password or private thing on Auberant. We are not responsible for any leaked personal information or deleted notes. :P`
   let messageDescription_nocontent = `Can't find any content to save.`
-  let messageDescription_saved = `Now you can find your note by using **;note open ${_id}**.`
+  let messageDescription_saved = `Saved as **#${_id}**.`
   let messageDescription_noid = `Can't interpret id to open.`
   let errorTitle = `Error`
   let _new = `new`
@@ -19,7 +19,7 @@ exports.run = (client, message, int, args, arg, perms, requestLanguage) => {
       messageTitle = `노트`
       messageDescription = `Auberant에서 노트를 써보세요. 먼저 노트를 만들어보세요: \`\`;노트 새로만들기 <내용>\`\`. 그리고 \`\`;노트 열기 <ID>\`\`로 열어보세요.\n\n노트는 Auberant가 업데이트할 때 언제든지 사라질 수 있습니다. 비밀번호 또는 개인정보를 Auberant에 저장하지 마세요. 저희는 유실된 노트 또는 개인정보에 대해 책임지지 않습니다. :P`
       messageDescription_nocontent = `저장할 내용을 찾을 수 없습니다.`
-      messageDescription_saved = `이제 저장된 노트를 **;노트 열기 ${_id}**로 찾을 수 있습니다.`
+      messageDescription_saved = `**#${_id}**로 저장되었습니다.`
       messageDescription_noid = `열기 위한 ID를 해석할 수 없습니다.`
       errorTitle = `오류`
       _new = `새로만들기`
